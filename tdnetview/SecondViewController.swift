1e32b1cc371ae8a39db86b1899bf00a300f057db
abars <abarsceo@gmail.com>
2015-04-11 13:17:46 +0900

    Initial Commit

---
+++ tdnetview/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tdnetview
+//
+//  Created by abars on 2015/04/11.
+//  Copyright (c) 2015年 abars. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
